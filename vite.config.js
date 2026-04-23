846717dc7a163117d0fe5c1395c8d80f27b29980
Leticia Santos <leticia.n.santos@gmail.com>
2026-04-23 16:49:24 -0300

    Fix port

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: { port: 8080 },
-  preview: { port: 8080 },
+  server: {
+    port: parseInt(process.env.PORT) || 8080,
+    host: '0.0.0.0',
+  },
+  preview: {
+    port: parseInt(process.env.PORT) || 8080,
+    host: '0.0.0.0',
+  },
 })
